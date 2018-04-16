79ae71c2ee0aa723853a8c436ea01d9d62c5ed34
Gautier VANNESTE <gvannest@e1r7p1.42.fr>
2018-04-16 12:21:06 +0200

    lemin.h gv

--- lem_in.h
+++ lem_in.h
@@ -6,7 +6,7 @@
 /*   By: apoque   <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 16:04:38 by gvannest          #+#    #+#             */
-/*   Updated: 2018/04/16 11:08:19 by gvannest         ###   ########.fr       */
+/*   Updated: 2018/04/16 12:20:34 by gvannest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ typedef struct					s_room
 				int				*bonded_rooms; // liste des salles adjascentes
 				int				occupied; //0 si libre sinon numero de la fourmi
 				char			visited; //1 :si salle visitee dans algo  du chemin plus court sinon 0
+				int				nb_steps; //nb de coups min depuis la salle start
 				struct s_room	**next_map; // tableau de pointeurs sur les salles next
 				struct s_room	*next_short_path; //pointeur sur next salle du chemin le plus court
 }								t_room;
@@ -41,7 +42,8 @@ typedef struct					s_room
 typedef struct					s_game
 {
 				t_ant			*ants; // liste des fourmis
-				t_room			*rooms; // debut de la liste chainee des salles
+				t_room			*room_start; // debut de la liste chainee des salles
+				t_room			*room_end; // fin de la liste chainee des salles
 				int				**paths; // tableau des n chemins les plus cours
 				int				nb_rooms; // nombre total de salle
 				int				nb_ants; // nombre total de fourmi
