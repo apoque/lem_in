233be809460890843134e26b3476c9c8ca2fd067
Gautier VANNESTE <gvannest@e1r9p5.42.fr>
2018-04-27 17:00:29 +0200

    new_path norme

--- srcs/new_path.c
+++ srcs/new_path.c
@@ -6,7 +6,7 @@
 /*   By: gvannest <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/24 13:47:49 by gvannest          #+#    #+#             */
-/*   Updated: 2018/04/27 15:59:32 by srossi           ###   ########.fr       */
+/*   Updated: 2018/04/27 16:59:31 by gvannest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,17 @@
 
 int		ft_new_shortpath(t_game *game, t_pile *current, t_ways **list_ways)
 {
-	t_ways *new_path;
-	//t_ways *ptr;
+	t_ways	*new_path;
 	int		k;
-	//int		i;
 
 	k = 0;
 	while (current->path[k])
 		k++;
-	if(!(new_path = ft_memalloc(sizeof(*new_path))))
-		return (-1);
-	if(!(new_path->sh_path = ft_memalloc(sizeof(t_room*) * (k + 2))))
+	if (!(new_path = ft_memalloc(sizeof(*new_path))) ||
+		!(new_path->sh_path = ft_memalloc(sizeof(t_room*) * (k + 2))))
 		return (-1);
 	ft_memcpy(new_path->sh_path, current->path, sizeof(t_room*) * (k + 2));
-	if(!(new_path->way = ft_memalloc(sizeof(int) * (k + 3))))
+	if (!(new_path->way = ft_memalloc(sizeof(int) * (k + 3))))
 		return (-1);
 	k = 0;
 	while (new_path->sh_path[k])
@@ -40,9 +37,6 @@ int		ft_new_shortpath(t_game *game, t_pile *current, t_ways **list_ways)
 	if (*list_ways != 0)
 		new_path->next = *list_ways;
 	*list_ways = new_path;
-	printf("A\n");
 	ft_compatibilities(game, list_ways, game->n);
-	printf("B\n");
 	return (0);
 }
-
