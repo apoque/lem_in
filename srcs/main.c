b8cf3f12a117b2665d2bcfbb8f830f63da9185e9
Amaury POQUE <apoque@e1r5p4.42.fr>
2018-05-01 10:43:22 +0200

    update main parsing

--- srcs/main.c
+++ srcs/main.c
@@ -6,42 +6,24 @@
 /*   By: srossi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/11 18:07:28 by srossi            #+#    #+#             */
-/*   Updated: 2018/04/30 19:33:03 by gvannest         ###   ########.fr       */
+/*   Updated: 2018/05/01 10:06:13 by apoque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-
-/*static int ft_display_ants(t_game *game)
-{
-	t_ant *ptr;
-
-	ptr = game->ants;
-	while (ptr)
-	{
-		ft_putnbr(ptr->nb);
-		ft_putchar('-');
-		ptr = ptr->next;
-	}
-	return (0);
-}*/
-
-int	main(void)
+int	ft_get_info(t_game *game)
 {
 	char	*line;
-	t_game	game;
 	int		i;
-	t_ways	*list_ways;
 
 	i = 0;
-	ft_bzero(&game, sizeof(t_game));
 	while (get_next_line(0, &line) && i == 0)
 	{
-		i = ft_parse(line, &game);
+		i = ft_parse(line, game);
 		if (i == 0)
 		{
-			ft_add_line(line, &game);
+			ft_add_line(line, game);
 			if (line)
 				ft_strdel(&line);
 		}
@@ -49,44 +31,28 @@ int	main(void)
 			return (-1);
 		else if (i == -1)
 		{
-			ft_display_lines(&game);
+			ft_display_lines(game);
 			return (-1);
 		}
 	}
-	ft_display_lines(&game);
-	if (ft_last_check(&game) == -2)
+	ft_display_lines(game);
+	if (ft_last_check(game) == -2)
 		return (-1);
-	ft_putchar('\n');
-	k = 0;
-	game.n = 2;
-	if (!(game.set.set = (int **)malloc(sizeof(int *) * game.n)))
-		exit(EXIT_FAILURE);
-	while (k < game.n)
-	{
-		m = 0;
-		if (!(game.set.set[k] = (int *)malloc(sizeof(int) * (game.nb_rooms + 1))))
-			exit(EXIT_FAILURE);
-		while (m <= game.nb_rooms)
-		{
-			game.set.set[k][m] = -3;
-			m++;
-		}
-		k++;
-	}
-	if (ft_last_check(&game) == -2)
+	return (1);
+}
+
+int	main(void)
+{
+	t_game	game;
+	t_ways	*list_ways;
+
+	ft_bzero(&game, sizeof(t_game));
+	if (ft_get_info(&game) < 0)
 		return (-1);
-	game.set.nb_path = 0;
-	game.set.cost = 0;
-	game.set.found = 0;
-	if(!(list_ways = ft_short_path(&game)))
+	if (!(list_ways = ft_short_path(&game)))
 		return (-1);
 	ft_select_path(&game);
 	ft_display_res(&game);
 	ft_free_game(&game, list_ways);
-//	ft_display_ants(&game);
-//	ft_free_lines(game.lines);
-//	ft_free_rooms(game.rooms);
-//	ft_free_ants(game.ants);
-//	ft_free_game(&game);
 	return (0);
 }
